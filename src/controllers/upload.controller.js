b21000abf1f548c5c7349a3ef416a97f45485904
Chris Mittell <mittell@hotmail.co.uk>
2022-10-31 14:21:08 +0900

    feat: updated controller logic

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,10 +1,21 @@
 const { ErrorHandler } = require('../utils/errorHandler');
+const { uploadToCloudinary } = require('../services/upload.service');
+const { bufferToDataURI } = require('../utils/file');
 
 const uploadImage = async (req, res, next) => {
 	try {
+		const { file } = req;
+		if (!file) throw new ErrorHandler(400, 'Image is required');
+
+		const fileFormat = file.mimetype.split('/')[1];
+		const { base64 } = bufferToDataURI(fileFormat, file.buffer);
+
+		const imageDetails = await uploadToCloudinary(base64, fileFormat);
+
 		res.json({
 			status: 'success',
 			message: 'Upload successful',
+			data: imageDetails,
 		});
 	} catch (error) {
 		next(new ErrorHandler(error.statusCode || 500, error.message));
